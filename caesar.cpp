0309e932bf2d8cb9722c164f896b11657cdb2110
0xstderr <113940514+0xstderr@users.noreply.github.com>
2022-10-13 17:08:52 +0000

    Create caesar.cpp

---
+++ caesar.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main() {
+
+    std::string text;
+
+    std::cout << "Please enter what to encrypt : ";
+    std::cin >> text;
+
+    std::vector<char> alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    std::vector<char> result;
+
+    int shift = 3;
+
+
+    for(int i = 0; i < text.size(); i++) {
+
+        for(int j = 0; j < alphabet.size(); j++) {
+
+            if(text[i] == alphabet[j]) {
+
+                result.push_back(alphabet[j + shift]);
+            }
+        }
+
+
+    }
+
+    for(int i = 0; i < result.size(); i++) {
+
+        std::cout << result[i];
+    }
+
+    std::cout << "\n";
+    return 0;
+
+}
